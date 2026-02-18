1a8b6f75ae6cd3390b7c5a0989b785db43de1066
Hazo Baikulov <spacehaz@gmail.com>
2026-02-18 14:51:49 +0300

    added updates after methods check

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,16 +1,10 @@
-import shortenString from './shorten-string';
-import defineApiUrl from './define-api-url';
 import api from './api';
 import createQueryString from './create-query-string';
-import msToTime from './ms-to-time';
 import isValidInboundMessage from './validate-inbound-message';
 import isValidOutboundMessage from './validate-outbound-message';
 import fetchRegistryConfig from './fetch-registry-config'
 
 export {
-  shortenString,
-  msToTime,
-  defineApiUrl,
   api,
   createQueryString,
   isValidInboundMessage,
--- src/utils/shorten-string.tsx
+++
@@ -1,9 +0,0 @@
-const shortenString = (string: string | undefined, symbolsToShow = 5) => {
-  if (!string) return '...';
-  if (string.length <= symbolsToShow) {
-    return string;
-  }
-  return `${string.slice(0, symbolsToShow)}...${string.slice(-symbolsToShow)}`;
-};
-
-export default shortenString;
